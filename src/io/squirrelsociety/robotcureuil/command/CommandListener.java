a71b213d51af419472f6ba486d853b5cfd0bca3d
zarychristo <32581220+zarychristo@users.noreply.github.com>
2017-10-06 21:53:16 +0200

    Update CommandListener.java

--- src/io/squirrelsociety/robotcureuil/command/CommandListener.java
+++ src/io/squirrelsociety/robotcureuil/command/CommandListener.java
@@ -1,12 +1,9 @@
 package io.squirrelsociety.robotcureuil.command;
 
 import java.awt.Color;
-import java.util.List;
-
 import net.dv8tion.jda.core.EmbedBuilder;
 import net.dv8tion.jda.core.Permission;
 import net.dv8tion.jda.core.entities.Message;
-import net.dv8tion.jda.core.entities.Role;
 import net.dv8tion.jda.core.entities.TextChannel;
 import net.dv8tion.jda.core.events.Event;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
@@ -53,23 +50,23 @@ public class CommandListener implements EventListener {
         }
     }
     
-    private void onReactionAdd(MessageReactionAddEvent event) {
-        Message message = event.getChannel().getMessageById(event.getMessageId()).complete();
-        System.out.println("auteur " + message.getAuthor().getName());
-        if(message.getAuthor().getName().equals("RobotCureuil")) {
-            if(message.getContent().equals("Clique sur le 🐿  pour avoir le rôle !")) {
-                System.out.println("Reaction added");
-                if(event.getUser().equals(event.getJDA().getSelfUser()))return;
-                if(event.getReactionEmote().getName().equals("🐿")) {
-                    if(event.getMember().getEffectiveName().endsWith("cureuil")) {
-                        System.out.println("Role to add : " + event.getGuild().getRoleById("364102814076633092").getName());
-                        event.getGuild().getController().addRolesToMember(event.getMember(), event.getGuild().getRoleById("364102814076633092")).queue();
-                        System.out.println("Role added to " + event.getMember().getEffectiveName());
-                    }
-                    else errorBuilder(event.getTextChannel(), "Votre pseudo ne finit pas par `cureuil`");
-                }
-            }
-           
-        }
+	private void onReactionAdd(MessageReactionAddEvent event) {
+		Message message = event.getChannel().getMessageById(event.getMessageId()).complete();
+		if(message.getAuthor().getId().equals("363797424734732289")) {
+			if(message.getContent().equals("Clique sur le 🐿  pour avoir le rôle !")) {
+				System.out.println("Reaction added");
+		    	if(event.getUser().equals(event.getJDA().getSelfUser()))return;
+		    	if(event.getReactionEmote().getName().equals("🐿")) {
+		    		if(event.getMember().getEffectiveName().endsWith("cureuil")) {
+		    			System.out.println("Role to add : " + event.getGuild().getRoleById("364102814076633092").getName());
+		    			event.getGuild().getController().addRolesToMember(event.getMember(), event.getGuild().getRoleById("364102814076633092")).queue();
+		    			System.out.println("Role added to " + event.getMember().getEffectiveName());
+		    		}
+		    		else errorBuilder(event.getTextChannel(), "Votre pseudo ne finit pas par `cureuil`");
+		    	}
+			}
+			
+		}
+		
     }
 }
